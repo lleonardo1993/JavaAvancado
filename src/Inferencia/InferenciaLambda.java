d3c60a70fdb0ce5b8d6d006a91cf069e72bee778
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2021-07-30 19:44:49 -0300

    validação de String com espaco.

--- src/Inferencia/InferenciaLambda.java
+++ src/Inferencia/InferenciaLambda.java
@@ -4,9 +4,9 @@ import java.util.function.Function;
 
 public class InferenciaLambda {
     public static void main(String[] args) {
-        Function<Integer,Double> divisaoPor2 = (var numero) -> numero/2.0;
+    String espaco = "                   ";
+        System.out.println(espaco == null || espaco.length() == 0 || espaco.chars().allMatch( c -> c == ' '));
 
-        System.out.println(divisaoPor2.apply( 9848322));
-
+    //true
     }
 }
