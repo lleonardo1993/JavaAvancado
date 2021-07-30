28d2db789686e45ce7e4b350a84c930e0ae54edc
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2021-07-30 19:46:08 -0300

    validação de String com espaco java 11 .

--- src/Inferencia/InferenciaLambda.java
+++ src/Inferencia/InferenciaLambda.java
@@ -5,8 +5,7 @@ import java.util.function.Function;
 public class InferenciaLambda {
     public static void main(String[] args) {
     String espaco = "                   ";
-        System.out.println(espaco == null || espaco.length() == 0 || espaco.chars().allMatch( c -> c == ' '));
-
+        System.out.println(espaco.isBlank());
     //true
     }
 }
