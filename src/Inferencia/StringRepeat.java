ca1efc1067e7af5e2a9d7e16eac25ed446e721d3
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2021-07-30 20:11:28 -0300

    // Codigo antigo Repeat String

---
+++ src/Inferencia/StringRepeat.java
@@ -0,0 +1,15 @@
+package Inferencia;
+
+public class StringRepeat {
+    public static void main(String[] args) {
+        String nome = "Leonardo";
+        String aux = "";
+
+        for (int i = 0; i < 10; i++) {
+            aux+=nome;
+        }
+        System.out.println(aux);
+
+        // Codigo antigo Repeat String
+    }
+}
